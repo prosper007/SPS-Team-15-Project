e6034236d1e64f10baf3fc2f2dea8d36ab7fe38d
duquej <johndduque@gmail.com>
2020-04-15 16:10:04 -0400

    delete functionality

---
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteRequestServlet.java
@@ -0,0 +1,90 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.Book;
+import com.google.sps.data.BookRequest;
+import java.util.Date;
+import com.google.sps.data.User;
+
+@WebServlet("/delete-request/*")
+public class DeleteRequestServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    //First find the specific request. If found, then check if the correct user is 
+    //trying to delete the request.
+    String requestKeyString = request.getPathInfo().substring(1);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    UserService userService = UserServiceFactory.getUserService();
+
+    Entity requestEntity;
+    Key requestKey = null;
+    try{
+      requestKey = KeyFactory.stringToKey(requestKeyString);
+      requestEntity = datastore.get(requestKey);
+    } catch(IllegalArgumentException | EntityNotFoundException e){
+      e.printStackTrace();
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+
+    Key userKey = (Key) requestEntity.getProperty("requester");
+    Entity userEntity = null;
+    
+    try{ 
+      userEntity = datastore.get(userKey);
+    } catch(EntityNotFoundException e){
+      e.printStackTrace();
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      return;
+    }
+
+    String email = (String) userEntity.getProperty("email");
+    String nickname = (String) userEntity.getProperty("nickname");
+    if(!userService.isUserLoggedIn()){
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+      response.sendRedirect("/");
+      return;
+    } 
+
+    if (nickname.equals(userService.getCurrentUser().getEmail())){
+        datastore.delete(requestKey);
+    }
+
+    response.sendRedirect("/index.html");
+
+
+  }
+}
